aeb4212a591710efaf51a2343acdf0ef347c9d49
Marcos <king.frontweb@outlook.com>
2023-05-25 14:12:27 -0300

    Att: Textos sobre nós

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -12,12 +12,12 @@ const AboutUs = () => {
           side="left"
           title="Nossa história"
           texts={[
-            `A Lordello Joias possui mais de 30 anos de tradição no mercado, sendo uma empresa com ampla experiência e especialização na compra de joias em ouro e platina, relógios e objetos em prata.`,
-            `Contamos com uma equipe de especialistas preparados para oferecer uma avaliação justa e segura, ajudando você a transformar suas peças em dinheiro de forma rápida e eficiente.`,
-            `Além disso, nosso compromisso com a transparência, lisura e segurança em nossas relações comerciais garante que cada cliente tenha uma experiência única e satisfatória conosco.`,
+            `Somos uma empresa que nasceu da paixão e tradição de uma família com mais de 30 anos de experiência no comércio de joias, relógios e objetos em prata. Além disso, desenvolvemos leilões e exposições de arte para trazer ainda mais exclusividade aos nossos clientes.`,
+            `Nossa filosofia é baseada na transparência, lisura e segurança em todas as nossas relações comerciais. Buscamos sempre oferecer um atendimento personalizado e um serviço excepcional para satisfazer as necessidades dos nossos clientes.`,
+            `Venha nos conhecer e descubra o encanto que as joias podem trazer para sua vida.`,
           ]}
         />
-        <AboveImage
+        {/* <AboveImage
           img=""
           side="right"
           title="Nossa equipe"
@@ -26,7 +26,7 @@ const AboutUs = () => {
             `Contamos com uma equipe de especialistas preparados para oferecer uma avaliação justa e segura, ajudando você a transformar suas peças em dinheiro de forma rápida e eficiente.`,
             `Além disso, nosso compromisso com a transparência, lisura e segurança em nossas relações comerciais garante que cada cliente tenha uma experiência única e satisfatória conosco.`,
           ]}
-        />
+        /> */}
       </div>
     </section>
   );
